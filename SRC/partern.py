a77016baeb63d56084ed8bb3eb6efdc09fbb2880
Arthur <arthur.courtrai@etu.univ-nantes.fr>
2024-01-26 16:47:48 +0100

    Amelioration du partern

--- SRC/partern.py
+++ SRC/partern.py
@@ -1,31 +1,34 @@
 import cv2
 import numpy as np
 from piece import piece
+from matplotlib import pyplot as plt
 
-image_path = 'BDD/image5.jpg' 
+image_path = 'BDD/image7.jpg' 
 
 reference_object_size_pixels = piece(image_path)
 
 # Les dimensions réelles en millimètres
 reference_object_real_world_mm = 22.6  # diamètre de la pièce de monnaie en mm
-diametre_reel_mm = 12  # Diamètre réel du filetage en mm
+diametre_reel_mm = 18  # Diamètre réel du filetage en mm
 pas_reel_mm = 1   # Pas réel du filetage en mm
 longueur_cm = 10  # Longueur des lignes horizontales en mm
+depassement_mm = 0.2 # Pas réel du filetage en mm
+
+pas_reel_mm = 25.4 / 14
 
 # Convertir les mesures réelles en pixels
 diametre_pixels = (diametre_reel_mm * reference_object_size_pixels) / reference_object_real_world_mm
 pas_pixels = (pas_reel_mm * reference_object_size_pixels) / reference_object_real_world_mm
 cm_pixels = (longueur_cm * reference_object_size_pixels) / reference_object_real_world_mm
+depassement_mm_pixel = (depassement_mm * reference_object_size_pixels) / reference_object_real_world_mm
 
+print(pas_pixels)
 # Créer une image noire qui servira de base pour le pattern
 pattern_height = int(diametre_pixels)  # Hauteur du pattern basée sur le diamètre en pixels
 pattern_width = int(cm_pixels)   # Largeur du pattern basée sur le pas en pixels
-
+depassement = int(depassement_mm_pixel) # Nombre de pixels que les verticales dépassent des horizontales
 
 pattern_image = np.zeros((pattern_height, pattern_width), dtype=np.uint8)
-
-depassement = 5  # Nombre de pixels que les verticales dépassent des horizontales
-
 
 # Dessiner les deux lignes horizontales
 cv2.line(pattern_image, (0, depassement), (pattern_width, depassement), (255), 1)
@@ -38,21 +41,25 @@ for x in range(0, pattern_width, int(pas_pixels)):
 # Sauvegarder l'image du pattern
 cv2.imwrite('Test/pattern_image.jpg', pattern_image)
 
-# # Charger l'image cible
-# target_image = cv2.imread(image_path)
-# gray_image = cv2.cvtColor(target_image, cv2.COLOR_BGR2GRAY)
 
-# # Effectuer le template matching
-# result = cv2.matchTemplate(gray_image, pattern_image, cv2.TM_CCOEFF_NORMED)
+#Phase de template
+img = cv2.imread('BDD/image7.jpg', cv2.IMREAD_GRAYSCALE)
+assert img is not None, "file could not be read, check with os.path.exists()"
+template = cv2.imread('Test/pattern_image.jpg', cv2.IMREAD_GRAYSCALE)
+assert template is not None, "file could not be read, check with os.path.exists()"
+w, h = template.shape[::-1]
 
-# # Seuil pour la reconnaissance
-# threshold = 0.8
-# locations = np.where(result >= threshold)
+# Effectuer le template matching
+res = cv2.matchTemplate(img,template,cv2.TM_CCOEFF_NORMED)
+min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 
-# # Superposer le pattern sur l'image cible
-# for loc in zip(*locations[::-1]):
-#     # Dessiner un rectangle autour de la zone correspondante
-#     cv2.rectangle(target_image, loc, (loc[0] + pattern_width, loc[1] + pattern_height), (0,255,0), 2)
+top_left = max_loc
+bottom_right = (top_left[0] + w, top_left[1] + h)
 
-# # Afficher l'image résultante
-# cv2.imwrite('Test/target_image.jpg', target_image)
+cv2.rectangle(img,top_left, bottom_right, 255, 2)
+plt.subplot(121),plt.imshow(res,cmap = 'gray')
+plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(img,cmap = 'gray')
+plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+
+plt.show()
