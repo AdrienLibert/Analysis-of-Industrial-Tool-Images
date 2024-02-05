f40b6bb534a10d17a249733a29ab4da3a5e0b3a8
Arthur <arthur.courtrai@etu.univ-nantes.fr>
2024-02-05 10:23:27 +0100

    Ajout rotation du pattern

--- SRC/tooth_pattern.py
+++ SRC/tooth_pattern.py
@@ -6,25 +6,25 @@ from diametre_monnaie import conversion_piece
 
 image_path = 'DataBase/image7.jpg' 
 
-reference_object_mm = 22.6  # diamètre de la pièce de monnaie en mm
+reference_object_mm = 22.6  # diamètre de la pièce de monnaie en mm 
 
-# Les dimensions réelles en millimètres de la piece
+# Les dimensions réelles en millimètres de la piece 
 
 diametre_reel_mm = 20  # Diamètre réel du filetage en mm  
 pas_reel_mm = 25.4 / 14 # Pas réel du filetage en mm
 
 
-def draw_tooth_pattern(image_path,diametre_reel_mm, pas_reel_mm, reference_object_mm):
+def draw_tooth_pattern(image_path,diametre_reel_mm, pas_reel_mm, reference_object_mm,rotation):
     size_pixels = conversion_piece(image_path, reference_object_mm)
     
     pas_pixels = pas_reel_mm / size_pixels
-    tooth_height = int(1 / size_pixels)  # Hauteur de la dent
+    tooth_height = int(1 / size_pixels)  # La hauteur de la dent est définie ici
     
     pattern_width = int(pas_pixels) * 4  # Largeur pour 4 dents
     
     pattern_image = np.zeros((tooth_height, pattern_width), dtype=np.uint8)
     
-    # Dessiner les 4 dents
+    # Dessiner 4 dents
     for i in range(4):
         # Calculer les points de départ, milieu et fin pour chaque dent
         start_x = int(i * pas_pixels + pas_pixels * 0.25)
@@ -34,8 +34,12 @@ def draw_tooth_pattern(image_path,diametre_reel_mm, pas_reel_mm, reference_objec
         # Dessiner les côtés supérieurs des dents
         cv2.line(pattern_image, (start_x, tooth_height - 1), (middle_x, 0), (255), 1)
         cv2.line(pattern_image, (middle_x, 0), (end_x, tooth_height - 1), (255), 1)
-    
+
+    if rotation:
+        pattern_image = cv2.rotate(pattern_image, cv2.ROTATE_180)
+        
     return pattern_image
+
 
 pattern_image = draw_tooth_pattern(image_path,diametre_reel_mm,pas_reel_mm,reference_object_mm)
 cv2.imwrite('pattern_image.jpg', pattern_image)
