fd7009c7755761053ba050a92c8ef9b50ce51635
E229866E <adrien.libert@etu.univ-nantes.fr>
2024-04-06 14:47:37 +0200

    diametre qui fonctionne

--- SRC/interface/diametre_interface.py
+++ SRC/interface/diametre_interface.py
@@ -1,7 +1,7 @@
 import math
 from PyQt5.QtWidgets import QLabel, QApplication
 from PyQt5.QtGui import QPainter, QPen, QPixmap
-from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtCore import Qt, QPoint, QLineF
 from viewer import ImageViewer
 from diametre_monnaie import conversion_piece
 
@@ -18,6 +18,8 @@ class PointDrawer(ImageViewer):
         self.points = []
 
     def addPoint(self, x, y):
+        if len(self.points) >= 2:
+            self.points.pop(0)
         self.points.append(QPoint(x, y))
         self.update()
 
@@ -28,17 +30,22 @@ class PointDrawer(ImageViewer):
         painter.setPen(pen)
         for point in self.points:
             painter.drawPoint(point)
+        if len(self.points) == 1:
+            line_end_point = QPoint(self.width(), self.points[0].y())
+            painter.drawLine(QLineF(self.points[0], line_end_point))
+        elif len(self.points) == 2:
+            painter.drawLine(QLineF(self.points[0], self.points[1]))
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
             self.addPoint(event.x(), event.y())
             if len(self.points) > 1:
                 distance_pixels = self.calculateDistance()
-                displayed_width = self.size().width()
-                resize_factor = displayed_width / self.original_width
-                distance_real = distance_pixels / resize_factor
-                size_pixels = conversion_piece(image_path, reference_object_mm)
-                distance_mm = distance_real * size_pixels
+                displayed_image_width = self.pixmap.scaled(self.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation).width() # Largeur de l'image affiché
+                resize_factor = displayed_image_width / self.original_width # Calcule le facteur de redimensionnement en divisant la largeur de l'image affichée par la largeur originale de l'image.              
+                distance_real = distance_pixels / resize_factor # Convertir la distance en pixels mesurée sur l'image redimensionnée en distance "réelle" en pixels sur l'image originale.
+                size_pixels = conversion_piece(image_path, reference_object_mm) #Fonction de conversion
+                distance_mm = distance_real * size_pixels # Calcul distance final
                 print(f"Distance entre les deux derniers points : {distance_mm} mm")
 
     def calculateDistance(self):
